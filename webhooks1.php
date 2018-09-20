a342b65e4edc986d1bcb542e5283ddaa1c5ff09c
thananan27 <thananan27@gmail.com>
2018-09-21 01:17:04 +0700

    Update webhooks1.php

--- webhooks1.php
+++ webhooks1.php
@@ -18,7 +18,7 @@
    if($message == "สวัสดี"){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = .$Id;
+      $arrayPostData['messages'][0]['text'] = ".$Id";
       $arrayPostData['messages'][1]['type'] = "sticker";
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
